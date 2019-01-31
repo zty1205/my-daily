429f56ab7c8bf5102cf8e2cfac2d208b1071bd08
zty <zhangtengyue@qbb6.com>
2019-01-31 16:39:27 +0800

    test mongoose

--- server/api/dailyMoney.js
+++ server/api/dailyMoney.js
@@ -11,8 +11,35 @@ router.get('/find', (req, res, next) => {   // 路由为 http://localhost:4000/m
             list: dm,
             length: dm.length
         })
-    }).catch(() => {
-        next()
+    }).catch((err) => {
+        res.json({
+            result: 1,
+            error: err
+        })
+    })
+})
+
+router.get('/add', (req, res, next) => {   // 路由为 http://localhost:4000/money/add
+    let one = {
+        expense: -998,
+        time:  "2019/1/1",
+        status: 1,
+        remark: "jello"
+    }
+    dailyMoney.create(one, (err, dm) => {
+        console.log("dm = ", dm)
+        // res.send(dm)
+        if (!err) {
+            res.json({
+                success: 0,
+                result: dm
+            })
+        } else {
+            res.json({
+                error: 1,
+                msg: err
+            })
+        }
     })
 })
 
--- server/db/schemas/dailyMoney.js
+++ server/db/schemas/dailyMoney.js
@@ -4,13 +4,24 @@ const Schema = mongoose.Schema;
 const STATUS_NORMAL = 1;
 const STATUS_DELETE = 0;
 
+const t = 1;
+
 const dailyMoney = new Schema({
-    mid: Number,
-    expense: Number,
-    time: Date,
+    // 结构定义
+    // id 就用 ObjectId
+    expense: { type: Number, required: true },
+    time:  { type: Date, required: true },
     status: { type: Number, default: STATUS_NORMAL },
-    createTime: { type: Date, default: Date.now },
-    remark: String
+    remark: { type: String, trim: true}
+}, {
+    // 配置项
+    collection: 'myDaily',
+    versionKey: false,
+    timestamps: { // 自动生成创建时间和更新时间
+      createdAt: true,
+      updatedAt: true,
+    },
+    strict: true // 不是在scheme定义的属性不允许插入到数据库
 });
 // 第三个参数是 MongoDB具体的 Collection Name 
 // 如果没有第三个参数 据说是查找的是dailyMoneys 的 Collection 但是死都查不到数据 还是添加第三个参数比较稳健
